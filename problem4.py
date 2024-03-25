ada5cb3d43161cb1638a322c5c0cc3e647e95ff3
Emily Sturman <emily@sturman.org>
2024-03-25 15:52:08 -0500

    Fix problem 4

--- problem4.py
+++ problem4.py
@@ -7,11 +7,11 @@ l1 = np.sqrt(47.5 ** 2 + (76 - 12) ** 2)
 l2 = 14.0
 l3 = 80.0
 l4 = 51.26
-theta2 = np.arctan2(76 - 12, 47.5)
+theta2 = np.degrees(np.arctan2(76 - 12, 47.5))
 
 
 def f(theta):
-    angles = np.degrees(utils.vector_loop(l1, l2, l3, l4, np.radians(theta2 - theta))) % 360 - theta2
+    angles = 180.0 - np.degrees(utils.vector_loop(l1, l2, l3, l4, np.radians(180.0 - theta2 - theta))) % 360 - theta2
     return np.vstack([angles[0], utils.get_transmission_angle(angles[1], angles[0])]).T
 
 
